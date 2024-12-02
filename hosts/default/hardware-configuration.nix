7eebc3cfbdacde6aa06aed9fb5396b3ebe24a92f
Tim Aschhoff <tim.aschhoff@t-online.de>
2024-12-02 18:03:41 +0100

    Remove old swap entry

--- hosts/default/hardware-configuration.nix
+++ hosts/default/hardware-configuration.nix
@@ -79,10 +79,6 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/938cbaca-0493-4eba-96c6-6622f2c33781"; }
-    ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
