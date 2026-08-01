fc47297cd13feeabdd96935f32682c136b9c22c2
Tim Aschhoff <tim@aschhoff.de>
2026-08-01 13:25:09 +0200

    Hide more messages for SimplePC

--- modules/hosts/chromebook-like/configuration.nix
+++ modules/hosts/chromebook-like/configuration.nix
@@ -23,6 +23,16 @@
     # Enable plymouth to hide the "scary" boot logs
     boot.plymouth.enable = true;
 
+    # Enable "Silent boot" (See https://wiki.nixos.org/wiki/Plymouth and https://wiki.archlinux.org/title/Silent_boot)
+    # IF a system fails to boot, using `e` on a boot entry in grub allows removing the quiet flags
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "rd.udev.log_level=3"
+      "rd.systemd.show_status=auto"
+    ];
+
 
     services.xserver.xkb.layout = "de";
     console.keyMap = "de";
