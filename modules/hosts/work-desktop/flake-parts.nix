4016b5ffc683576d879451cc2dab6873480b2f80
Tim Aschhoff <tim@aschhoff.de>
2026-08-18 09:35:38 +0200

    add home-manager config

--- modules/hosts/work-desktop/flake-parts.nix
+++ modules/hosts/work-desktop/flake-parts.nix
@@ -4,4 +4,5 @@
 }:
 {
   flake.nixosConfigurations = inputs.self.lib.mkNixos "x86_64-linux" "Tims-Work-Desktop";
+  flake.homeConfigurations = inputs.self.lib.mkHomeManager inputs.nixpkgs.legacyPackages."x86_64-linux".stdenv "tim-work";
 }
